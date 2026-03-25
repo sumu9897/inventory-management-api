77f795eaaa3aa5d2a34796c584638dca2c4bb03d
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2026-03-25 10:47:48 +0600

    feat: integrate item routes and middleware into app

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
-const express = require('express');
+const express = require("express");
+const itemRoutes = require("./routes/itemRoutes");
+const notFound = require("./middlewares/notFound");
+const errorHandler = require("./middlewares/errorHandler");
 
 const app = express();
 
+// Middleware
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Welcome to the Inventory Management API!');
+// Home route
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Welcome to the Inventory Management API!"
+  });
 });
+
+// Routes
+app.use("/api/items", itemRoutes);
+
+// Not found middleware
+app.use(notFound);
+
+// Error handler middleware
+app.use(errorHandler);
 
 module.exports = app;
