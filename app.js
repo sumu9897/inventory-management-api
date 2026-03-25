414ee2ed0ba07781fc307336eb39d68293fce29d
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2026-03-25 11:16:28 +0600

    feat: enable CORS for frontend integration

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require("express");
 const itemRoutes = require("./routes/itemRoutes");
 const notFound = require("./middlewares/notFound");
 const errorHandler = require("./middlewares/errorHandeler");
+const cors = require("cors");
 
 
 const app = express();
 
 // Middleware
 app.use(express.json());
+app.use(cors());
 
 // Home route
 app.get("/", (req, res) => {
